bdccccaf1ca8dc7051d451ba65ab91a96d411516
Nurtilek-se22 <sergeinurtilek@gmail.com>
2025-10-01 01:46:30 +0500

    Delete Mp4Player.java

--- Mp4Player.java
+++
@@ -1,7 +0,0 @@
-// MP4 Player
-public class Mp4Player implements MediaPlayer {
-    @Override
-    public void play(String fileName) {
-        System.out.println("Playing MP4 file: " + fileName);
-    }
-}
