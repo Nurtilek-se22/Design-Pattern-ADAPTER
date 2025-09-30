9b48ca999ab22d926fadccd41a00cca7524d6fd2
Nurtilek-se22 <sergeinurtilek@gmail.com>
2025-10-01 01:45:53 +0500

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        AudioPlayer audioPlayer = new AudioPlayer();
-
-        audioPlayer.play("song.mp3", "mp3");
-
-        audioPlayer.play("movie.mp4", "mp4");
-    }
-}
