6ec029a0278fb5a6b07a8da070aaad3cbbd96544
Nurtilek-se22 <sergeinurtilek@gmail.com>
2025-10-01 11:43:21 +0500

    Delete FifaLeague.java

--- FifaLeague.java
+++
@@ -1,6 +0,0 @@
-// Adaptee 2 (FIFA)
-public class FifaLeague {
-    public void presentMatch(String clubA, String clubB) {
-        System.out.println("🌍 FIFA Match: " + clubA + " vs " + clubB);
-    }
-}
