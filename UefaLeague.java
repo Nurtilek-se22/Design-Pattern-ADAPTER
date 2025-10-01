0da9b08ffd95573f6441eb5ae52208cfe29ecc15
Nurtilek-se22 <sergeinurtilek@gmail.com>
2025-10-01 11:43:56 +0500

    Delete UefaLeague.java

--- UefaLeague.java
+++
@@ -1,6 +0,0 @@
-// Adaptee 1 (UEFA League)
-public class UefaLeague {
-    public void displayGame(String team1, String team2) {
-        System.out.println("🏆 UEFA League Match: " + team1 + " vs " + team2);
-    }
-}
