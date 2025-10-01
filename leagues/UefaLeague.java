18d8dbac7d4a5057e5afdf2e14dc5926d3e97631
Nurtilek-se22 <sergeinurtilek@gmail.com>
2025-10-01 12:18:31 +0500

    Update UefaLeague.java

--- leagues/UefaLeague.java
+++ leagues/UefaLeague.java
@@ -3,6 +3,6 @@ package leagues;
 // Adaptee 1 (UEFA League)
 public class UefaLeague {
     public void displayGame(String team1, String team2) {
-        System.out.println("🏆 UEFA League Match: " + team1 + " vs " + team2);
+        System.out.println("UEFA League Match: " + team1 + " vs " + team2);
     }
 }
